28130fa1aa3042d52ebed4eecb23c430138c061c
wiraphat2711 <wiraphat.a@ku.th>
2021-10-30 12:36:26 +0700

    project V1

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
                             <a class="nav-link" href="/kid">KIDS</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/test1">CONTACT</a>
+                            <a class="nav-link" href="/receipt">CONTACT</a>
                         </li>
                     </ul>
                 </div>
--- src/components/works/receipt.vue
+++ src/components/works/receipt.vue
@@ -1,13 +1,33 @@
 <template>
-  <h1>re</h1>
+  <header class="container">
+      
+       <img src="https://images.pexels.com/photos/3965545/pexels-photo-3965545.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="img-fluid" alt="..." width="700px" height="300px">
+ <h2 align="center">แสดงความคิดเห็นของคุณที่นี่</h2>
+<div class="mb-3 ">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">comment</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+
+
+  </div>
+
+
+<button class="btn btn-danger" @click="save(product)">ยืนยัน</button>
+
+
+ </header>
+
+
+
 </template>
 
 <script>
 export default {
-
-}
+  name: "App",
+};
 </script>
-
-<style>
-
+<style scoped>
 </style>
